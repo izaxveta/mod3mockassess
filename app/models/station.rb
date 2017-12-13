422a942234747b9eef02e2fa30a5d642ffe2411d
izaxveta <izavetha@gmail.com>
2017-12-13 09:43:14 -0700

    Api works via Postman, setting up test working on view and back.

---
+++ app/models/station.rb
@@ -0,0 +1,8 @@
+class Station < ApplicationRecord
+
+
+  def method_name
+
+  end
+
+end
--- spec/features/user_can_visit_root_page_spec.rb
+++ spec/features/user_can_visit_root_page_spec.rb
@@ -8,4 +8,13 @@ feature "User can visit root page" do
       expect(page).to have_selector("input[value='Search by zip...']")
     end
   end
+
+  scenario "and can utilize search tool" do
+    visit '/'
+    # fill_in "[]", with: "80203"
+    click_on "Locate"
+
+    expect(current_path).to eq('/search')
+  end
+
 end
